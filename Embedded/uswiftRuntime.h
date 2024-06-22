0c57a3864212acdcccaf6e95911248e4ef1bad11
Carl Peto <CPeto@becrypt.com>
2024-06-22 14:25:20 +0100

    update shims

--- Embedded/uswiftRuntime.h
+++ Embedded/uswiftRuntime.h
@@ -1,4 +1,5 @@
 _Bool main_loop_running();
 
 // noop by default, can be overridden
-void _microswift_array_out_of_bounds(int index, int count, void * _storage);
+void _microswift_array_out_of_bounds(int* index, int lowerBound, int upperBound);
+void _microswift_class_creation_attempt_oom(int size);
